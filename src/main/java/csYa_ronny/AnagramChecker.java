4353e992af2a5e640659b9fd96494a14e9775f13
giladEilat <gilad.eilat@gmail.com>
2024-11-09 16:48:53 +0200

    Update AnagramChecker.java

--- src/main/java/csYa_ronny/AnagramChecker.java
+++ src/main/java/csYa_ronny/AnagramChecker.java
@@ -3,7 +3,16 @@ package csYa_ronny;
 public class AnagramChecker {
     public static boolean areAnagrams(String str1, String str2) {
         // TODO: Implement the logic to check if str1 and str2 are anagrams
-        return false;  // Return false by default
+        int sum1=0,sum2=0;
+        for (int i = 0; i < str1.length(); i++) {
+            sum1+=str1.charAt(i);
+        }
+        for (int i = 0; i < str2.length(); i++) {
+            sum2+=str2.charAt(i);
+        }
+        if (sum1==sum2)
+            return true;
+        return false; //אני בטוח שזה לא עובד
     }
 
     public static void main(String[] args) {
